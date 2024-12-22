82948e40d4d99c06c454c46ffbc37a8a51f9700e
Souleymane DIALLO <sdley2020@gmail.com>
2024-12-22 10:58:00 +0000

    Built Home page logic and structure

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import '@styles/globals.css';
-// import { Children } from 'react';
+
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 export const metadata = {
     title: 'sdley Promptupai',
@@ -13,7 +15,9 @@ const RootLayout = ({ children }) => {
             <div className='main'>
                 <div className="gradient" />
             </div>
+
             <main className='app'>
+                <Nav />
                 {children}
             </main>
         </body>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+// module.exports = {
+export default {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
